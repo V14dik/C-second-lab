83653676c497e81ce8f160fd7d98e8f737df8155
V14dik <62301196+V14dik@users.noreply.github.com>
2020-04-01 23:20:15 +0300

    Add files via upload

---
+++ 2lab(2.1.27).cpp
@@ -0,0 +1,217 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void infmat()
+{
+    int *i = 0;
+    system("cls");
+    printf("Цемент-$230 за тонну\n");
+    printf("Гравий-$142 за тонну\n");
+    printf("Бетон-$260 за тонну\n");
+    printf("Нажмите любую клавишу для выхода в меню\n");
+    _getch();
+    return;
+}
+
+
+void info()
+{
+    int *i = 0;
+    system("cls");
+    printf("Программу составил Черенкевич В.А.\n");
+    printf("Версия программы №1\n");
+    printf("Нажмите любую клавишу для выхода в меню\n");
+    _getch();
+    return;
+}
+
+
+int kol (int wheaght)
+{
+    int  vag = 0;
+    if ((wheaght % 50) > 0)
+        vag = (wheaght / 50) + 1;
+    else vag = wheaght / 50;
+    return vag;
+}
+
+
+int country()
+{
+    system("cls");
+    int choice = 0;
+    printf("Выберете страну доставки\n");
+    printf("1.Беларусь\n");
+    printf("2.Россия\n");
+    printf("3.Украина\n");
+    printf("4.Молдавия\n");
+    if (scanf_s("%i", &choice) != 1)
+    {
+        rewind(stdin);
+        printf("Неверный ввод\n");
+    }
+    else 
+        while ((choice < 1) and (choice > 4))
+        {
+            rewind(stdin);
+            printf("Оформить заказ можно только в предложенные страны\n");
+            scanf_s("%i", &choice);
+        }
+    return choice;
+}
+
+int coastcountry(int ordercountry)
+{
+    switch (ordercountry)
+    {
+    case 1: return 200;
+    case 2: return 320;
+    case 3: return 320;
+    case 4: return 360;
+    }
+}
+int cem()
+{
+    system("cls");
+    int material = 0, sumord = 0, realsum = 0, resh = 0, wheaght = 0, ordercountry = 0, raz = 0;
+    printf("Введите количество цемента в тоннах\n");
+    if (scanf_s("%i", &wheaght) != 1)
+    {
+        rewind(stdin);
+        printf("Неверный ввод");
+    }
+    sumord += wheaght * 230;
+    realsum += wheaght * 160;
+    ordercountry = country();
+    sumord += kol(wheaght) * coastcountry(ordercountry);
+    realsum += kol(wheaght) * coastcountry(ordercountry);
+    raz = sumord - realsum;
+    if (raz >= 25000)
+    {
+        system("cls");
+        printf("Общая стоимость заказа с учётом доставки составила:$%i\n", sumord);
+        printf("Прибыль предприятия:$%i\n", raz);
+    }
+    else
+    {
+        system("cls");
+        printf("Заказ не принят");
+    }
+    return 0;
+}
+
+int grav()
+{
+    system("cls");
+    int material = 0, sumord = 0, realsum = 0, resh = 0, wheaght = 0, ordercountry = 0, raz = 0;
+    printf("Введите количество гравия в тоннах\n");
+    if (scanf_s("%i", &wheaght) != 1)
+    {
+        rewind(stdin);
+        printf("Неверный ввод");
+    }
+    sumord += wheaght * 142;
+    realsum += wheaght * 96;
+    ordercountry = country();
+    sumord += kol(wheaght) * coastcountry(ordercountry);
+    realsum += kol(wheaght) * coastcountry(ordercountry);
+    if (raz >= 25000)
+    {
+        system("cls");
+        printf("Общая стоимость заказа с учётом доставки составила:$%i\n", sumord);
+        printf("Прибыль предприятия:$%i\n", raz);
+    }
+    else
+    {
+        system("cls");
+        printf("Заказ не принят");
+    }
+    return 0;
+}
+
+int bet()
+{
+    int material = 0, sumord = 0, realsum = 0, resh = 0, wheaght = 0, ordercountry = 0, raz = 0;
+    system("cls");
+    printf("Введите количество бетона в тоннах\n");
+    if (scanf_s("%i", &wheaght) != 1)
+    {
+        rewind(stdin);
+        printf("Неверный ввод");
+    }
+    sumord += wheaght * 260;
+    realsum += wheaght * 205;
+    ordercountry = country();
+    sumord += kol(wheaght) * coastcountry(ordercountry);
+    realsum += kol(wheaght) * coastcountry(ordercountry);
+    if (raz >= 25000)
+    {
+        system("cls");
+        printf("Общая стоимость заказа с учётом доставки составила:$%i\n", sumord);
+        printf("Прибыль предприятия:$%i\n", raz);
+    }
+    else
+    {
+        system("cls");
+        printf("Заказ не принят");
+    }
+    return 0;
+}
+
+int menu()
+{
+    m:
+    printf("1.Заказ на цемент\n");
+    printf("2.Заказ на гравий\n");
+    printf("3.Заказ на бетон\n");
+    printf("4.Информация о материалах\n");
+    printf("5.Информация о пограмме\n");
+    printf("6.Выход из программы\n");
+    int material = 0, raz = 0;
+    if (scanf_s("%i", &material) != 1)
+    {
+        rewind(stdin);
+        printf("Неверный ввод\n");
+    }
+    while ((material < 1) or (material > 6))
+    {
+        rewind(stdin);
+        printf("Неверный ввод(введите заново)\n");
+        scanf_s("%i", &material);
+    }
+    switch (material)
+    {
+    case 1: cem(); break;
+    case 2: grav(); break;
+    case 3: bet(); break;
+    case 4:
+    {
+        infmat();
+        system("cls");
+        goto m;
+    }
+    case 5:
+    {
+        info();
+        system("cls");
+        goto m;
+    }
+    case 6: return 0;
+    }
+    return 0;
+
+}
+
+
+int main()
+{
+    int raz=0;
+    system("chcp 1251");
+    system("cls");
+    menu();
+    return 0;
+
+}
---
+++ 2lab(2.2.6).cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#define _USE_MATH_DEFINES
+#include<cmath>
+
+int main(void)
+{
+    float eps;
+    printf("Enter epsilon: ");
+    while (scanf_s("%f", &eps) != 1 || eps >= 1 || eps <= 0)
+    {
+        rewind(stdin);
+        printf("Bad input.\nEnter epsilon: ");
+    }
+
+    int angle;//в градусах
+    printf("Enter angle in degerees: ");
+    while (scanf_s("%d", &angle) != 1)
+    {
+        rewind(stdin);
+        printf("Bad input.\nEnter angle in degrees: ");
+    }
+
+    int n = 1;
+    float x = angle * M_PI / 180, a = sin(x), b = x, fact_num = 1, factor = x;
+    while (fabsf(a - b) >= eps)
+    {
+        printf("n=%d is too small.\n", n);
+        printf("sin(x)=%.18f\ttailor_sin(x)=%.18f\n", a, b);
+        n++;
+        fact_num += 2;
+        factor = factor * x * x / fact_num / (fact_num - 1);
+        if (n & 1)
+            b += factor;
+        else
+            b -= factor;
+    }
+    printf("Seems like n=%d is enough.\n", n);
+    printf("sin(x)=%.18f\ttailor_sin(x)=%.18f\n", a, b);
+
+    return 0;
+}
